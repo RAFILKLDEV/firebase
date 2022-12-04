dc6ed1bd69c9d93168944f7a5e929ba3bac01bb9
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-12-03 21:24:50 -0300

    Added function GetUsers Firebase!

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import {
   collection,
   deleteDoc,
   doc,
+  updateDoc,
+  Firestore,
   getDocs,
   getFirestore,
 } from "firebase/firestore";
@@ -12,17 +14,69 @@ import { useEffect, useState } from "react";
 
 function App() {
   const firebaseApp = initializeApp({
-    apiKey: "AIzaSyDANRLrZ_DEVDebPo3OmCCZ0lg5i1ev6WU",
-    authDomain: "biblioteca-t20.firebaseapp.com",
-    projectId: "biblioteca-t20",
+    apiKey: "AIzaSyBPap_2XBwhRNU7ejE4ZRbcxyMotEN7xGI",
+    authDomain: "tormenta-3582c.firebaseapp.com",
+    projectId: "tormenta-3582c",
+    storageBucket: "tormenta-3582c.appspot.com",
+    messagingSenderId: "508691522674",
+    appId: "1:508691522674:web:b3b6f251c8cffed9130eeb",
+    measurementId: "G-YL6K5SZXRP",
   });
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [sheets, setSheets] = useState([]);
   const [users, setUsers] = useState([]);
 
   const db = getFirestore(firebaseApp);
   const userCollectionRef = collection(db, "users");
+
+  const registerSheet = async (name, nd, image) => {
+    const object = new Object();
+    object.name = name;
+    object.nd = nd;
+    object.image = image;
+    console.log(object);
+    setSheets([object]);
+  };
+
+  const SheetCreator = () => {
+    const [name, setName] = useState("");
+    const [nd, setNd] = useState(0);
+    const [image, setImage] = useState("");
+    return (
+      <div>
+        <div>
+          <div>Nome do Npc: </div>
+          <input value={name} onChange={(e) => setName(e.target.value)}></input>
+        </div>
+        <div>
+          <div>Nd: </div>
+          <input
+            value={nd}
+            onChange={(e) => setNd(e.target.value)}
+            type="number"
+          ></input>
+        </div>
+        <div>
+          <div>Image: </div>
+          <input
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            type="text"
+          ></input>
+          <img src={image} width={"100%"} alt={image} />
+        </div>
+        <button
+          onClick={() => {
+            registerSheet(name, nd, image);
+          }}
+        >
+          Registrar Sheet!
+        </button>
+      </div>
+    );
+  };
 
   const showUsers = () => {
     return users.map((e) => (
@@ -31,33 +85,42 @@ function App() {
           Usuario: {e.name} <button onClick={() => deleteUser(e.id)}>X</button>
         </div>
         <div>Email: {e.email}</div>
+        <div>Sheets</div>
+        <div>{e.sheets.name}</div>
       </div>
     ));
   };
 
   const registerUser = () => {
     const register = async () => {
-      const user = await addDoc(userCollectionRef, {
+      await addDoc(userCollectionRef, {
         name,
         email,
+        sheets,
       });
-      console.log(user);
     };
     return (
       <div>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <div>
+          <div>Usuário: </div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <div>Email: </div>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
         <button
           onClick={() => {
             register();
+            getUsers();
           }}
         >
           Registrar
@@ -69,20 +132,55 @@ function App() {
   const deleteUser = async (id) => {
     const userDoc = doc(db, "users", id);
     await deleteDoc(userDoc);
+    getUsers();
+  };
+
+  const getUsers = async () => {
+    const data = await getDocs(userCollectionRef);
+    await setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    console.log(data.docs[0].data());
+  };
+
+  const updateUser = async () => {
+    const docRef = doc(db, "users", "adYM8ULHHbandLgIb4MB");
+    await updateDoc(docRef, {
+      name: "anderson",
+      email: "rafa",
+      sheets,
+    });
   };
 
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(userCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+    console.log("Get!");
     getUsers();
-  }, [users]);
+  }, []);
 
   return (
     <div className="App">
-      <div>{showUsers()}</div>
-      <div>{registerUser()}</div>
+      <div className="Tabela">
+        <h1>Usuários</h1>
+        <div>{showUsers()}</div>
+      </div>
+      <div className="Tabela">
+        <h1>Registrar Usuários</h1>
+        <div>{registerUser()}</div>
+      </div>
+      <div className="Tabela">
+        <h1>Registrar Ficha de Usuário</h1>
+        <div>{SheetCreator()}</div>
+      </div>
+      <div className="Tabela">
+        <h1>Alterar Usuário</h1>
+        <div>
+          <button
+            onClick={() => {
+              updateUser();
+            }}
+          >
+            Update
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
